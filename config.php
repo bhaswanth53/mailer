b4656e0d49556b9ceaf9d6657e6d9e4fef732aa1
bhaswanth97 <bhaswanth.nexevo@gmail.com>
2019-08-31 12:30:07 +0530

    Modified config

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    return array(
+        "MAIL_DRIVER" => "smtp",
+        "MAIL_HOST" => "your_mail_host",
+        "MAIL_PORT" => "your_mail_post",
+        "MAIL_USERNAME" => "your_email",
+        "MAIL_PASSWORD" => "your_password",
+        "MAIL_ENCRYPTION" => "your_mail_encryption"
+    );
--- mail/mail.php
+++ mail/mail.php
@@ -153,7 +153,7 @@
 			$var=ob_get_contents(); 
 			ob_end_clean();
             return $var; */
-            $config = include("credentials.php");
+            $config = include("config.php");
             return $config;
 		}
     }
